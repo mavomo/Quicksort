c4985a8422d50d8b7d5ebd352267f0e12882ac03
msherefel <michelle.avomo@gmail.com>
2014-05-25 12:00:29 +0200

    javadoc

--- src/main/java/org/coursera/algorithm/design/quicksort/Quicksort.java
+++ src/main/java/org/coursera/algorithm/design/quicksort/Quicksort.java
@@ -11,7 +11,8 @@ import java.io.IOException;
 public class Quicksort {
 	
 	public static void main(String[] args) throws IOException {
-		int[] inputData = {5,3,6,4,8,0,1};
+		//int[] inputData = {5,3,6,4,8,0,1};
+		int[] inputData = {6,5,4,3,2,1,0};
 		
 		for(int i=0; i< inputData.length; i++){
 			System.out.println(inputData[i]);
@@ -21,17 +22,15 @@ public class Quicksort {
 		
 		for(int i=0; i< inputData.length; i++){
 			System.out.println(inputData[i]);
-		}
-
-		
+		}		
 	}
 	
 	public static int[] partitionizeAndSort(int[] inputArray, int startIndex, int endIndex){
-		//General case 
-		//Position of the partition index
+		//Base case
 		if (endIndex-1 <= startIndex){
 			return inputArray;
 		}else{
+			//General case
 			int nouveauPivot = partitionize(inputArray, startIndex, endIndex);
 			partitionizeAndSort(inputArray, startIndex, nouveauPivot);	
 			partitionizeAndSort(inputArray, nouveauPivot, endIndex);	
@@ -40,23 +39,29 @@ public class Quicksort {
 		
 	}
 	
+	/**
+	 * Partitionning method that takes the first element of the array as its pivot
+	 * @param inputArray : array to be sorted
+	 * @param start: first element of the array
+	 * @param end : last element in the array
+	 * @return an array containing the pivot at its right position with all the lower numbers in the left and all the upper ones in the left of the pivot
+	 */
 	private static int partitionize(int[] inputArray, int start, int end){
-		//At first, we consider the pivot as the first entry of our array.
-		  
+		//The pivot corresponds to the first element of the Array		  
 			int pivot=inputArray[start];							
 			//Boundary of the latest element <p
-			int innerCounter = start+1;	
-				for(int i=start+1; i< end; i++){
-					//If the current input is lower that the pivot then swap pivots.
-					if(i < inputArray.length){
-						 if(inputArray[i] <= pivot){
-							   int tmp = inputArray[i];
-							  inputArray[i] = inputArray[innerCounter];
-							  inputArray[innerCounter] = tmp;	
-							  innerCounter++;
-						}
-					}				
+		int innerCounter = start + 1;
+		for (int i = start + 1; i < end; i++) {
+			// If the current input is lower that the pivot then swap pivots.
+			if (i < inputArray.length) {
+				if (inputArray[i] <= pivot) {
+					int tmp = inputArray[i];
+					inputArray[i] = inputArray[innerCounter];
+					inputArray[innerCounter] = tmp;
+					innerCounter++;
 				}
+			}
+		}
 		//Putting the pivot at the right position
 		int tmp = inputArray[start]; 
 		inputArray[start] = inputArray[innerCounter-1];
